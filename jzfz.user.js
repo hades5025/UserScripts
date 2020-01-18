2ed163096cbb9b54bf1678504fa715f69fa25a6f
ConnersHua <ConnersHua@me.com>
2020-01-18 20:36:42 +0800

    🚩 增加基准方中

---
+++ jzfz.user.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         基准方中文档去水印
+// @namespace    https://github.com/DivineEngine/UserScripts
+// @version      0.1
+// @description  移除基准方中文档的水印
+// @author       DivineEngine
+// @match        *://wiseweb.jzfz.com.cn/*
+// @grant        none
+// @updateURL    https://raw.githubusercontent.com/DivineEngine/UserScripts/master/jzfz.user.js
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    waterMarkConfig.showWaterMark = false;
+})();
